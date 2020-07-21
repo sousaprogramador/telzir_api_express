cbeb9fe40beecad7bfc0026bd8065dd561c69bb0
Mateus <sousa.programador@gmail.com>
2020-07-21 15:47:19 -0300

    test: ensure PlansControllers returns missinr params erro

--- src/presentation/controllers/plan.spec.ts
+++ src/presentation/controllers/plan.spec.ts
@@ -1,16 +1,30 @@
 import { PlansController } from './plansController'
 describe('Plans Controller', () => {
-  test('Should return 400 if no ddds is provided', () => {
+  test('Should return 400 if no ddd_origin is provided', () => {
     const sut = new PlansController()
     const httpRequest = {
       body: {
-        ddd_origin: 'any_ddd',
         ddd_destination: 'any_ddd',
         plan_minutes: 'any_minutes'
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing params: ddd_origin'))
+  })
+})
+
+describe('Plans Controller', () => {
+  test('Should return 400 if no ddd_destination is provided', () => {
+    const sut = new PlansController()
+    const httpRequest = {
+      body: {
+        ddd_origin: 'any_ddd',
+        plan_minutes: 'any_minutes'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing params: ddd_destination'))
   })
 })
