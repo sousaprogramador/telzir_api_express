90589d4d29e48684510b8b3d45cbb8e0527f3fdb
Mateus <sousa.programador@gmail.com>
2020-07-27 09:59:35 -0300

    refactor: move duplicate code to a help

--- src/presentation/controllers/plansController.ts
+++ src/presentation/controllers/plansController.ts
@@ -1,19 +1,14 @@
 import { HttRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-params-error'
+import { badResquest } from '../helpers/http-helper'
+
 export class PlansController {
   handle(httpRequest: HttRequest): HttpResponse {
     if (!httpRequest.body.ddd_origin) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('ddd_origin')
-      }
+      return badResquest(new MissingParamError('ddd_origin'))
     }
-
     if (!httpRequest.body.ddd_destination) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('ddd_destination')
-      }
+      return badResquest(new MissingParamError('ddd_destination'))
     }
   }
 }
---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,5 @@
+import { HttpResponse } from '../protocols/http'
+export const badResquest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
