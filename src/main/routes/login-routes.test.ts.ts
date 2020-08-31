c58fd6bc850395821835a3fd79a2f269c93a66b7
Mateus Sousa <sousa.programador@gmail.com>
2020-08-31 14:27:51 -0300

    refactor: connect to mongo db

--- src/main/routes/login-routes.test.ts.ts
+++ src/main/routes/login-routes.test.ts.ts
@@ -1,7 +1,24 @@
 import request from 'supertest'
+import { Collection } from 'mongodb'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '@/main/config/app'
 
+let accountCollection: Collection
+
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
